86a78bdadd8ea4fd15e33d9af6324db9d141567b
bheintz <benjamin.heintz@soprasterianext.com>
2020-04-08 23:56:20 +0200

    redirect hasura

--- Controllers/mvc/HomeController.cs
+++ Controllers/mvc/HomeController.cs
@@ -1,13 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace IronHasura.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly string hasuraUrl;
+
+        public HomeController(IConfiguration configuration)
+        {
+            this.hasuraUrl = configuration.GetValue<string>("IRONHASURA_CONSOLE_URL");
+        }
+
         [Route("/")]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Route("/home/data")]
+        public IActionResult Hasura()
+        {
+
+            return Redirect(this.hasuraUrl);
         }
     }
 }
