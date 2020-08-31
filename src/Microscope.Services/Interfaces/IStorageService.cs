f50df95ec12a7fb479de155bc235518852b09754
bheintz <benjamin.heintz@soprasterianext.com>
2020-08-31 18:23:30 +0200

    refactoring solution & project name

---
+++ src/Microscope.Services/Interfaces/IStorageService.cs
@@ -0,0 +1,43 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Microscope.Services.Interface
+{
+    public interface IStorageService
+    {   
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        Task<string> SaveFile(string filename, IFormFile file);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        Task<bool> DeleteFile(string filename);
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        Task<string> GetFileUri(string filename);
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        Task<byte[]> GetFileData(string filename);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        Task<string[]> GetFiles();
+    }
+}
